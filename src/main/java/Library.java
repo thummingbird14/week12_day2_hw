4a393b49cb49734d36e5e9265d67e68c695eb67d
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-05-10 21:44:40 +0100

    Commit MVP

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,16 +3,39 @@ import java.util.ArrayList;
 public class Library {
 
     private ArrayList<Book> collection;
+    private int capacity;
 
-    public Library(){
+    public Library(int capacity){
+        this.capacity = capacity;
         this.collection = new ArrayList<>();
     }
 
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
     public int collectionSize(){
-        return this.collection.size();
+        return collection.size();
+    }
+
+    public boolean stockIsFull(){
+        if (collectionSize() >= capacity)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public void addBook(Book book){
-        this.collection.add(book);
+        if (!stockIsFull()){
+            this.collection.add(book);
+        }
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -9,13 +9,20 @@ public class LibraryTest {
     private Book book1;
     private Book book2;
     private Book book3;
+    private Book book4;
 
     @Before
     public void before(){
-        library = new Library();
+        library = new Library(500);
         book1 = new Book("The Eyre Affair", "Jasper FForde", "Fantasy");
         book2 = new Book("Jane Eyre", "Charlotte Bronte", "Classics");
         book3 = new Book("He Said, She Said", "Erin Kelly", "Thriller");
+        book4 = new Book("Imogen", "Jilly Cooper", "Romance");
+    }
+
+    @Test
+    public void libraryHasCapacity(){
+        assertEquals(500, library.getCapacity());
     }
 
     @Test
@@ -28,6 +35,16 @@ public class LibraryTest {
         library.addBook(book1);
         library.addBook(book2);
         library.addBook(book3);
+        assertEquals(3, library.collectionSize());
+    }
+
+    @Test
+    public void cannotAddBooksWhenNoCapacity(){
+        library.setCapacity(3);
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
         assertEquals(3, library.collectionSize());
     }
 
